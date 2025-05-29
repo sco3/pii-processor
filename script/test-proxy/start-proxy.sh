9ad369105dce010640e6839ef7cc9c150bd64130
Dmitry Zakharov <dmitry.zakharov@caelus-technologies.com>
2025-05-29 12:43:18 +0100

    test

--- script/test-proxy/start-proxy.sh
+++ script/test-proxy/start-proxy.sh
@@ -15,12 +15,22 @@ else
 	source $env
 fi
 
-tmux new -d -s litellm uv run litellm --debug --config config.yaml
+#tmux new -d -s litellm uv run litellm --debug --config config.yaml
+
+docker run -d  \
+    -v $(pwd)/config.yaml:/app/config.yaml \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    -e AWS_REGION=$AWS_REGION \
+    -p 4000:4000 \
+    --name litellm \
+    ghcr.io/berriai/litellm:main-latest \
+    --config /app/config.yaml --detailed_debug 
 
 clear
 while true; do 
-   out=$(netstat -ltnp | grep -Eo ':4000\b' || echo Not yet )
-   if [ "$out" == ":4000" ]; then
+   out=$(netstat -ltnp | grep -Eo ":4000\b" | head -n 1 | tr -d ' ' || echo Not yet )
+   if [ "$out" == ":4000"  ]; then
       break
    fi
    sleep 1
--- script/test-proxy/test-curl.sh
+++ script/test-proxy/test-curl.sh
@@ -1,23 +1,24 @@
-#!/usr/bin/env -S bash -x 
-
+#!/usr/bin/env -S bash -x
 
 curl --location 'http://0.0.0.0:4000/v1/models' \
-    --header 'Authorization: Bearer sk-1234' \
-    --header 'Content-Type: application/json' | yq .data[0].id
+	--header 'Authorization: Bearer sk-1234' \
+	--header 'Content-Type: application/json' | yq .data[0].id
+	
+	
+msg=
 
-
-curl --location 'http://0.0.0.0:4000/chat/completions' \
-    --header 'Authorization: Bearer sk-1234' \
-    --header 'Content-Type: application/json' \
-    --data '{
+time curl -s 'http://0.0.0.0:4000/chat/completions' \
+	--header 'Authorization: Bearer sk-1234' \
+	--header 'Content-Type: application/json' \
+	--data '{
     "model": "claude-3-haiku-proxy",
     "messages": [
 	{ "role":"system",
-	  "content":"The user presents the quotation from the novel for the purpose of PII redaction abilities test. Extract names, places, dates and other PII from the given message. No text message to the uesr only extracted data in json format."
+	  "content":"The user presents the text for personal identifiable information redaction. Extract names, places, dates, identifiers and other PII from the given message. Yaml result should an array with no tags with colons only plain strings .  No text message to the uesr only extracted data in yaml format."
 	},
         {
         "role": "user",
-        "content": "Hello I am Fantomas and tried to to escape commissaire Paul Juve with police badge number 42 in Paris in 1964."
+        "content": "Hello I am Fantomas and tried to to escape commissaire Paul Juve with police badge number 42 in Paris in 1964. A phone number +44-2244 was in use lately."
         }
     ]
-}' | yq -P 
+}' | yq -P .choices[0].message.content
