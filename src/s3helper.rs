251501012f04d564b816c23848b2adc4d4f2f8de
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-06-05 04:43:15 +0100

    s3

--- src/s3helper.rs
+++ src/s3helper.rs
@@ -1,25 +1,34 @@
 //use aws_config::meta::region::RegionProviderChain;
-use aws_config::{defaults, BehaviorVersion, Region};
+use aws_config::{BehaviorVersion, Region};
 
 use aws_credential_types::Credentials;
 use aws_sdk_s3::Client;
+use aws_sdk_s3::Config;
 use std::error::Error;
 use std::panic::{catch_unwind, AssertUnwindSafe};
-use tracing::error;
+use tracing::{error, info};
 
 pub struct S3Helper {
     pub s3: Option<Client>,
+    pub bucket: String,
 }
 
 impl S3Helper {
     pub async fn new(
+        bucket: String,
         region: String,
         access_key: Option<String>,
         secret_key: Option<String>,
         session_token: Option<String>,
+        endpoint_url: Option<String>,
     ) -> Result<Self, Box<dyn Error>> {
-        let mut config_loader =
-            defaults(BehaviorVersion::latest()).region(Region::new(region.clone()));
+        let mut s3_config_builder = Config::builder()
+            .behavior_version_latest()
+            .region(Region::new(region));
+
+        if let Some(url) = endpoint_url {
+            s3_config_builder = s3_config_builder.endpoint_url(url);
+        }
 
         if let (Some(ak), Some(sk)) = (access_key.clone(), secret_key.clone()) {
             let credentials = Credentials::new(
@@ -29,15 +38,12 @@ impl S3Helper {
                 None,
                 "Static",
             );
-            config_loader = config_loader
-                .credentials_provider(credentials)
-                .region(Region::new(region.clone()));
+            s3_config_builder = s3_config_builder.credentials_provider(credentials);
         }
-
-        let conf = config_loader.load().await;
+        let s3_conf = s3_config_builder.build();
 
         let s3 = match catch_unwind(
-            AssertUnwindSafe(|| Client::new(&conf)), //
+            AssertUnwindSafe(|| Client::from_conf(s3_conf)), //
         ) {
             Ok(cli) => Some(cli),
             Err(e) => {
@@ -46,6 +52,18 @@ impl S3Helper {
             }
         };
 
-        Ok(S3Helper { s3 })
+        Ok(S3Helper { bucket, s3 })
+    }
+
+    pub async fn list_buckets(&self) {
+        if let Some(s3) = &self.s3 {
+            let mut buckets = s3.list_buckets().into_paginator().send();
+            while let Some(Ok(output)) = buckets.next().await {
+                for bucket in output.buckets() {
+                    let name = bucket.name().unwrap_or_default();
+                    info!("Bucket: {}", name);
+                }
+            }
+        }
     }
 }
--- tests/test_s3.rs
+++ tests/test_s3.rs
@@ -1,6 +1,7 @@
 mod common;
 
 pub use common::init_logging::init_tracing;
+use ductaper::s3helper::S3Helper;
 use reqwest::StatusCode;
 use testcontainers::core::wait::HttpWaitStrategy;
 use testcontainers::{
@@ -29,11 +30,24 @@ async fn test_s3() {
             .with_expected_status_code(StatusCode::OK),
     ))
     .with_network("bridge")
+    .with_env_var("initialBuckets", "test-bucket")
     .start()
     .await
-    .expect("Failed to start Nats");
+    .expect("Failed to start s3");
 
     if let Ok(port) = container.get_host_port_ipv4(9090.tcp()).await {
         info!("Port {port}");
+        if let Ok(s3) = S3Helper::new(
+            "test".to_string(),
+            "eu-west-1".to_string(), //
+            None,
+            None,
+            None,
+            Some(format!("http://localhost{port}")),
+        )
+        .await
+        {
+            s3.list_buckets().await;
+        }
     }
 }
