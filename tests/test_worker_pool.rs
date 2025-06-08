d22dc9bba4212a1927ee224e80a8a0c15204dd06
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-06-08 15:03:14 +0100

    pool

--- tests/test_worker_pool.rs
+++ tests/test_worker_pool.rs
@@ -1,16 +1,50 @@
+mod common;
 use async_channel::bounded;
 use async_nats::jetstream::Message;
 use ductaper::worker_pool::WorkerPool;
-use tracing::debug;
+use reqwest::StatusCode;
+
+use tracing::{debug, info};
+
+use common::init_logging::init_tracing;
+
+use testcontainers::core::wait::HttpWaitStrategy;
+use testcontainers::{
+    core::{IntoContainerPort, WaitFor}, runners::AsyncRunner,
+    GenericImage,
+    ImageExt,
+};
 
 #[tokio::test]
 async fn test_pool() {
-    let (tx, rx) = bounded::<Message>(1);
-    debug!("Tx: {:?}", tx);
+    init_tracing();
+    // unsafe {
+    //     std::env::remove_var("DOCKER_HOST");
+    // }
 
-    let pool = WorkerPool {
-        size: 1,
-        receiver: rx,
-    };
-    pool.start().await;
+    let container = GenericImage::new("nats", "2.11.4")
+        .with_exposed_port(4222.tcp())
+        .with_wait_for(WaitFor::http(
+            HttpWaitStrategy::new("/healthz")
+                .with_port(8222.tcp()) //
+                .with_expected_status_code(StatusCode::OK),
+        ))
+        .with_network("bridge")
+        .with_cmd(["-js", "-m", "8222"])
+        .start()
+        .await
+        .expect("Failed to start Nats");
+
+    if let Ok(port) = container.get_host_port_ipv4(4222.tcp()).await {
+        info!("Container port: {port}");
+
+        let (tx, rx) = bounded::<Message>(1);
+        debug!("Tx: {:?}", tx);
+
+        let pool = WorkerPool {
+            size: 1,
+            receiver: rx,
+        };
+        pool.start().await;
+    }
 }
