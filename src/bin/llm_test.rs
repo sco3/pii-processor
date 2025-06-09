015815fc888874362f281e3d039efe2000e3a959
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-06-09 10:00:46 +0100

    test llm call

--- src/bin/llm_test.rs
+++ src/bin/llm_test.rs
@@ -1,6 +1,8 @@
 use ductaper::init_logging::init_tracing;
 use ductaper::llm_caller::LLmCaller;
 use ductaper::reducter::ReDucter;
+use std::env::var;
+use std::fs::read_to_string;
 use std::time::Instant;
 use tracing::info;
 
@@ -10,23 +12,52 @@ const TOKEN: &str = "sk-1234";
 #[tokio::main]
 async fn main() {
     init_tracing();
-    let models = vec!["nova", "haiku"];
-    for model in &models {
-        call_with_model(model, "Hello", "Hi").await;
+
+    let models = vec![/*"nova",*/ "haiku"];
+
+    let system_prompt = read_to_string("data/system_prompt.txt") //
+        .unwrap();
+    let session_log = read_to_string("tests/data/worker-pool-test.json").unwrap();
+
+    if var("ASDF").unwrap_or_default() == "ASDF" {
+        simple_tests(&models, &system_prompt).await;
     }
 
     for model in &models {
         call_with_model(
             model, //
+            system_prompt.as_str(),
+            session_log.as_str(),
+        )
+        .await;
+    }
+}
+
+async fn simple_tests(models: &Vec<&str>, system_prompt: &String) {
+    for model in models {
+        call_with_model(model, "Hello", "Hi").await;
+    }
+
+    for model in models {
+        call_with_model(
+            model, //
             r#"
-        You are a PII redactor that only returns redacted text 
+        You are a PII redactor that only returns redacted text
         with no additional text or explanations.
         "#,
             "Hello I am Jack Daniels.",
         )
         .await;
     }
-    //
+
+    for model in models {
+        call_with_model(
+            model, //
+            system_prompt.as_str(),
+            "Hello I am Jack Daniels.",
+        )
+        .await;
+    }
 }
 
 async fn call_with_model(model: &str, prompt: &str, msg: &str) {
@@ -54,8 +85,12 @@ async fn response_details(caller: LLmCaller, model: &str, prompt: &str, msg: &st
             if let Some(choice_array) = choices.as_array() {
                 for choice in choice_array {
                     info!("Choice {:?}", choice);
-                    info!("Message {}", choice["message"]["content"]);
+                    let msg = choice["message"]["content"].clone();
+                    info!("----");
+                    info!("\n\nMessage {}\n\n", msg);
                     info!("----")
+
+                    // msg[""]
                 }
             }
         }
