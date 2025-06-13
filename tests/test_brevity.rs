42f21942de6b6689f1de4325a7419df887ee011c
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-06-13 10:07:33 +0100

    refactor

--- tests/test_brevity.rs
+++ tests/test_brevity.rs
@@ -1,10 +1,14 @@
 use ductaper::llm_work::brevity::brevify;
+use ductaper::logging::init_tracing;
+use tracing::info;
 
 #[test]
 pub fn test_brevity() -> Result<(), Box<dyn std::error::Error>> {
+    init_tracing();
     let content = std::fs::read_to_string(
         "tests/data/example_system_prompt.txt", //
     )?;
-    brevify(content.as_str());
+    let out = brevify(content.as_str());
+    info!("Briefly?: {:?}", out);
     Ok(())
 }
