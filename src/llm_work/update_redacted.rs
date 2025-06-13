56941689b22ff8e106f9217bfa3acf6684ca1c01
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-06-13 20:52:07 +0100

    update tool calls

--- src/llm_work/update_redacted.rs
+++ src/llm_work/update_redacted.rs
@@ -1,5 +1,5 @@
 //use crate::data::session_log_models::ChatMessage;
-use crate::data::session_log_models::SessionLog;
+use crate::data::session_log_models::{ChatMessageItem, SessionLog};
 use crate::llm_work::llm_log_processor::LlmLogProcessor;
 
 use crate::data::session_log_models::SessionLogEntry::{
@@ -13,7 +13,7 @@ impl LlmLogProcessor {
             match entry {
                 ChatMessageEnum(chat_msg) => {
                     chat_msg.content = Self::upd_field(chat_msg.content.clone(), redacts);
-                    //Self::process_tool_calls(chat_msg);
+                    Self::process_tool_calls(chat_msg, redacts);
                 }
                 ChatGptEnum(chat_gpt) => {
                     for chat_msg in &mut chat_gpt.chat_gpt.request.chat_history {
@@ -32,13 +32,30 @@ impl LlmLogProcessor {
         }
     }
 
-    // fn process_tool_calls(chat_msg: &mut ChatMessage) {
-    //     if let Some(tool_calls) = &chat_msg.tool_calls {
-    //         for tool_call in tool_calls {
-    //             let function = tool_call;
+    // possibly better option to parse arguments from string to json, update only values
+    // let args_result = serde_json::from_str::<Value>(args);
+    // if let Ok(args_value) = args_result {
+    //     if let Some(args_map) = args_value.as_object() {
+    //         for (_, val) in args_map {
+    //             if let Some(s) = val.as_str() {
+    //                 ... = Self::upd_field(s, redacts);
+    //
+    //             }
     //         }
     //     }
     // }
+
+    fn process_tool_calls(chat_msg: &mut ChatMessageItem, redacts: &HashMap<String, String>) {
+        if let Some(tool_calls) = &mut chat_msg.tool_calls {
+            for tool_call in tool_calls {
+                let function = &tool_call.function;
+                if let Some(args) = &function.arguments {
+                    let upd = Self::upd_field(args.clone(), redacts);
+                    tool_call.function.arguments = Some(upd);
+                }
+            }
+        }
+    }
 
     fn upd_field(old: String, redacts: &HashMap<String, String>) -> String {
         let mut content = old;
