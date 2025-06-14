c83749f62a94ddce153358cb148cd61c0be7954c
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-06-14 20:16:55 +0100

    tests

--- src/llm_work/prompt.rs
+++ src/llm_work/prompt.rs
@@ -1,10 +1,10 @@
 use std::fs::read_to_string;
-use tracing::error;
 
-pub fn read_prompt(location: &String) -> String {
-    read_to_string(location) //
-        .unwrap_or_else(|e| {
-            error!("Failed to read system prompt: {} {}", e, location);
-            String::new()
-        })
+pub fn read_prompt(location: &str) -> String {
+    read_to_string(location).unwrap_or_else(|e| {
+        panic!(
+            "Failed to read system prompt: {} {}", //
+            e, location
+        )
+    })
 }
