15471cd150ae736b0d413ea629e90724dfac819f
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-06-26 21:17:57 +0100

    s3 saver

--- src/storage/s3_saver.rs
+++ src/storage/s3_saver.rs
@@ -1,6 +1,7 @@
 use crate::config::env_vars::Cfg;
 use crate::data::session_log_models::SessionLog;
 use crate::probe::toggle::Toggle;
+use std::error::Error;
 
 use crate::storage::s3ctx::S3Ctx;
 use crate::storage::s3helper::S3Helper;
@@ -22,16 +23,28 @@ pub struct S3Saver {
 }
 
 impl S3Saver {
-    /// Constructor
-    ///
-    pub async fn new(bucket: &str, cfg: &Cfg, toggle: Toggle) -> Self {
-        info!("S3 saver init: {bucket}");
+    /// init s3 context
+    /// # Errors
+    /// returned when s3 context creation failed
+    pub async fn get_s3_ctx(bucket: &str, cfg: &Cfg) -> Result<S3Ctx, Box<dyn Error>> {
+        info!("S3 init: {bucket}");
 
-        let access_key: Option<String> = cfg.aws_access_key_id.clone().map(|v| v.get_string());
-        let secret_key: Option<String> = cfg.aws_secret_access_key.clone().map(|v| v.get_string());
-        let access_token: Option<String> = cfg.aws_access_token.clone().map(|v| v.get_string());
+        let access_key: Option<String> = cfg //
+            .aws_access_key_id
+            .clone()
+            .map(|v| v.get_string());
 
-        let s3ctx = match S3Ctx::new(
+        let secret_key: Option<String> = cfg //
+            .aws_secret_access_key
+            .clone()
+            .map(|v| v.get_string());
+
+        let access_token: Option<String> = cfg //
+            .aws_access_token
+            .clone()
+            .map(|v| v.get_string());
+
+        match S3Ctx::new(
             bucket.to_string(),
             cfg.aws_region_s3.clone(),
             access_key,
@@ -41,20 +54,26 @@ impl S3Saver {
         )
         .await
         {
-            Ok(ctx) => {
+            Ok(ctx) => Ok(ctx),
+            Err(e) => Err(e),
+        }
+    }
+    /// Constructor
+    ///
+    pub async fn new(bucket: &str, cfg: &Cfg, toggle: Toggle) -> Self {
+        match S3Saver::get_s3_ctx(bucket, cfg).await {
+            Ok(s3ctx) => {
                 toggle.set_ready(true);
-                ctx
+                S3Saver {
+                    s3helper: Some(S3Helper::new(s3ctx)),
+                    bucket: Arc::from(bucket),
+                    toggle,
+                }
             }
             Err(e) => {
                 error!("S3 problem: {}", e);
                 exit(ExitCode::S3Error.code());
             }
-        };
-
-        S3Saver {
-            s3helper: Some(S3Helper::new(s3ctx)),
-            bucket: Arc::from(bucket),
-            toggle,
         }
     }
 }
